95e8afc1967c77758c9323ae49e06ff13617a037
pross1312 <dangvinhtuong12@gmail.com>
2025-07-17 09:50:36 +0700

    complete p2p server

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 import (
-	"os"
 	"time"
-    "fmt"
-    "net"
+	"fmt"
+	"os"
+	"encoding/json"
+)
+var (
+	msg_format = `{
+	"message": "hello from %s!"
+}`
+	message string
 )
 
-func handleConnection(conn net.Conn) error {
-	for {
-		fmt.Fprintf(conn, "Hello bro\n")
-		time.Sleep(1)
+func handleWriting(channel chan string) {
+	for _ = range 100 {
+		olog(INFO, "Broadcasting message `", message, "`")
+		channel <- message
+		time.Sleep(2 * time.Second)
 	}
+}
+
+func handleNewMessage(message any) error {
+	bytes, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+
+
+	olog(INFO, "New message '", string(bytes), "'")
 	return nil
 }
 
@@ -23,12 +40,14 @@ func main() {
 	}
 
 	sv_addr := args[0]; args = args[1:]
-	
-
+	message = fmt.Sprintf(msg_format, sv_addr)
+	broad_cast_channel := make(chan string, DEFAULT_CHANNEL_BUFFER)
     server := P2PServer {
         Addr: sv_addr,
 		Peers: args,
-        Handler: handleConnection,
+        Handler: handleNewMessage,
+		BroadCastChannel : broad_cast_channel,
     }
+	go handleWriting(broad_cast_channel)
     server.Start()
 }
--- p2pserver.go
+++ p2pserver.go
@@ -1,6 +1,13 @@
 package main
 import (
+	"slices"
 	"net"
+	"time"
+	"encoding/json"
+)
+
+const (
+	DEFAULT_CHANNEL_BUFFER = 128
 )
 
 type P2PChannelData struct {string; error}
@@ -8,51 +15,118 @@ type P2PChannel chan P2PChannelData
 type P2PServer struct {
     Addr string
     Peers []string
-    Handler func(net.Conn) error
-}
+    Handler func(message any) error
+    BroadCastChannel chan string
 
-func (sv *P2PServer) handleClient(conn net.Conn, chann P2PChannel) {
-	defer conn.Close()
-	err := sv.Handler(conn)
-	chann <- P2PChannelData{conn.RemoteAddr().String(), err}
-}
-
-func (sv *P2PServer) handleServerConnection(conn net.Conn) {
-	defer conn.Close()
-	sv.Handler(conn)
-	olog(INFO, "Connection ", conn.RemoteAddr().String(), " closed!");
+	newConnChannel chan net.Conn
+	clientDeadChannel chan string
 }
 
 func (sv *P2PServer) connnectToPeers() {
-    channels := make(P2PChannel, len(sv.Peers) * 2)
-    defer close(channels)
-    for i := 0; i < len(sv.Peers); i++ {
-        channels <- struct {string; error}{sv.Peers[i], nil}
+	for _, id := range sv.Peers {
+		sv.clientDeadChannel <- id
     }
 
-    for data := range channels {
-        if data.error != nil {
-            olog(WARNING, "Error happen for ", data.string, ", error: ", data.error.Error())
-        }
-        olog(INFO, "Connecting to ", data.string, "...")
-        conn, err := net.Dial("tcp", data.string)
-        olog(INFO, "Connected to ", data.string)
-        if err != nil {
-            channels <- struct {string; error}{data.string, err}
-            continue
-        }
+	for client_dead_id := range sv.clientDeadChannel {
+		if !slices.Contains(sv.Peers, client_dead_id) {
+			continue
+		}
+		olog(INFO, "Connecting to ", client_dead_id, "...")
+		conn, err := net.Dial("tcp", client_dead_id)
+		if err != nil {
+			olog(WARNING, "Failed to connect to ", client_dead_id, ", error: ", err.Error())
+			go func(duration time.Duration, id string, done_channel chan string) {
+				time.Sleep(duration)
+				done_channel <- id
+			}(5000 * time.Millisecond, client_dead_id, sv.clientDeadChannel)
+			continue
+		}
+		sv.newConnChannel <- conn
+	}
+}
 
-        go sv.handleClient(conn, channels)
-    }
+func handleRead(id string, conn net.Conn, done_channel chan string, handler func(any) error) {
+	decoder := json.NewDecoder(conn)
+	var data any
+	for {
+		err := decoder.Decode(&data)
+		if err != nil {
+			olog(ERROR, "Error when reading json from connection, error: ", err.Error())
+			done_channel <- id
+			break
+		}
+		err = handler(data)
+		if err != nil {
+			olog(ERROR, "Error when handling message, error: ", err.Error())
+			done_channel <- id
+			break
+		}
+	}
+}
+
+func handleWrite(id string, conn net.Conn, write_channel chan string) {
+	for data := range write_channel {
+		_, err := conn.Write([]byte(data))
+		if err != nil {
+			olog(ERROR, "Failed to write '", data, "' to connection ", conn.RemoteAddr().String())
+		}
+	}
+}
+
+type Writer struct {
+	conn net.Conn
+	channel chan string
+}
+
+func (sv *P2PServer) handleBroadCast() {
+	done_channel := make(chan string, 10)
+	defer close(done_channel)
+	writers := make(map[string]Writer)
+	defer func(writers map[string]Writer) {
+		for _, writer := range writers {
+			close(writer.channel)
+			writer.conn.Close()
+		}
+	}(writers)
+	for {
+		select {
+		case conn := <- sv.newConnChannel:
+			id := conn.RemoteAddr().String()
+			olog(INFO, "New connection ", id);
+
+			write_channel := make(chan string, DEFAULT_CHANNEL_BUFFER)
+			writers[id] = Writer { conn, write_channel }
+
+			go handleRead(id, conn, done_channel, sv.Handler)
+			go handleWrite(id, conn, write_channel)
+		case message_to_sent := <- sv.BroadCastChannel:
+			for _, writer := range writers {
+				writer.channel <- message_to_sent
+			}
+		case done_id := <- done_channel:
+			olog(INFO, "Connection ", done_id, " closed!");
+			writer := writers[done_id]
+			close(writer.channel)
+			writer.conn.Close()
+			delete(writers, done_id)
+			sv.clientDeadChannel <- done_id
+		}
+	}
 }
 
 func (sv *P2PServer) Start() error {
+	sv.clientDeadChannel = make(chan string, DEFAULT_CHANNEL_BUFFER)
+	sv.newConnChannel = make(chan net.Conn, DEFAULT_CHANNEL_BUFFER)
+
+	go sv.handleBroadCast()
+
+	go sv.connnectToPeers()
+
     server, err := net.Listen("tcp", sv.Addr)
     if err != nil {
         return err
     }
     defer server.Close()
-	go sv.connnectToPeers()
     olog(INFO, "Listening on ", server.Addr().String())
     for {
         conn, err := server.Accept()
@@ -60,9 +134,7 @@ func (sv *P2PServer) Start() error {
             olog(ERROR, "Failed to accept connection, error ", err.Error())
             continue
         }
-
-        olog(INFO, "New connection ", conn.RemoteAddr().String())
-		go sv.handleServerConnection(conn)
+		sv.newConnChannel <- conn
     }
 }
 
